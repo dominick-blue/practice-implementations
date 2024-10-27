9a8c6674771cb259f72dfdce4e797499ac57f425
Dominick Blue <dominick.blue7@gmail.com>
2024-10-27 05:13:50 -0700

    Implementing initial method structures for a LinkedList class

---
+++ src/src/LinkedList.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+
+public class LinkedList
+{
+    public LinkedList()
+    {
+
+    }
+
+    public int get(int index)
+    {
+
+    }
+
+    public void insertHead(int val)
+    {
+
+    }
+
+    public void insertTail(int val)
+    {
+
+    }
+
+    public boolean remove(int index)
+    {
+
+    }
+
+    public ArrayList<Integer> getValues()
+    {
+
+    }
+}
